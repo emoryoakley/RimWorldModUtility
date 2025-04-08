d3f0d46c32c90695f4a432130a50e10c1de8dd4b
emoryoakley <emoryoakley.game@gmail.com>
2025-04-08 18:15:25 +0800

    feat: 首次上傳 CustomTranslator 工具

---
+++ CustomTranslator/Properties/VersionInfo.cs
@@ -0,0 +1,5 @@
+using System.Reflection;
+// GenerateVersion.dll 自動生成 VersionInfo.cs
+[assembly: AssemblyVersion("1.0.9229.943")]
+[assembly: AssemblyFileVersion("1.0.9229.943")]
+[assembly: AssemblyInformationalVersion("1.0.20250407")]
